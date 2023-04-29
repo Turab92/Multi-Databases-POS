dfd9ef53baabeb4b41c2a7029ce5360dd08152cc
turab haider <turab.haider@itsmstaging.com>
2023-04-29 23:58:48 +0500

    code updated

--- app/confiq/retailer_db.js
+++
@@ -1,6 +0,0 @@
-exports.finddb=(dbname)=>{
-    const finddb = require("../../app/individual/models/user");
-    const db=finddb.getdb(dbname)
-
-    return db
-  };
--- app/middleware/authentication.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(req, res, next) {
-    if(!req.cookies['auth-token']) {
-        return res.redirect('/login');
-    }
-return next();
-};
